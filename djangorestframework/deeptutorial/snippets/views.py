66d98b708f1b45fe116ec153cd779195e487f3e7
lenny kioko <lenny@lennys-MacBook-Pro.local>
2018-07-29 15:45:48 +0300

    Completed project code

--- djangorestframework/deeptutorial/snippets/views.py
+++ djangorestframework/deeptutorial/snippets/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+# pylint:disable=E1101
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer, UserSerializer
+from django.contrib.auth.models import User
+from rest_framework import permissions
+from snippets.permissions import IsOwnerOrReadOnly
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import renderers
+from rest_framework.decorators import action
 
-# Create your views here.
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    This viewset automatically provides `list` and `detail` actions.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class SnippetViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
+
+    Additionally we also provide an extra `highlight` action.
+    """
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly,)
+
+    # the @action decorator is used to add any custom endpoints that don't fit into the standard create/update/delete style.
+    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
